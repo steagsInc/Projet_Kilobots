0503408618ed7d1d713b4f68138c095cec3f40c3
Ouaguenouni Mohamed <ouaguenouni.hachemi@gmail.com>
2020-03-13 11:49:27 +0100

    Ajout de la Hinge Loss et de la Log-Loss

--- Module_Python/NeuralNetworksTest/interfaceNN.py
+++ Module_Python/NeuralNetworksTest/interfaceNN.py
@@ -4,7 +4,7 @@ import cma
 import numpy as np
 import pandas as pd
 import json
-
+from sklearn.metrics import log_loss,hinge_loss
 
 os.chdir('..')
 path_weights = "kilombo/templates/kilotron/weights.txt"
@@ -148,13 +148,60 @@ def testAccuracyFixedV2(w,borne = 2):
         c = 0
         for j in range(y_p.shape[0]):
             #Je compte une pénalité si l'avis du robot donc argmax(p) != de la vraie topologie
-
             if(np.argmax(y_p[j]) != i%2):
                 c= c + 1
         cpt = cpt + c
         print('Sanction pour une topologie : ',c)
     print("Pénalité totale : ",cpt)
     return cpt
+
+
+def testLogLoss(w,borne = 2):
+    tests = []
+    writeWeights(w)
+    L = []
+    cpt = 0
+    for i in range(0,borne):
+        if(i%2 == 0):
+            topology = "circle"
+        else:
+            topology = "line"
+        setTopology(topology,nb_bot_max)
+        simulatePerceptron(topology,nb_bot_max)
+        y_p = getPredictions()
+        Z = np.hstack((np.zeros((y_p.shape[0], 1)), np.ones((y_p.shape[0], 1))))
+        if(i%2 == 1):
+            Z = np.hstack((np.ones((y_p.shape[0],1)),np.zeros((y_p.shape[0],1)) ))
+        c=log_loss(Z,y_p)
+        cpt = cpt + c
+        print('Sanction pour une topologie : ',c)
+    print("Pénalité totale : ",cpt)
+    return cpt
+
+def testHingeLoss(w,borne = 2):
+    tests = []
+    writeWeights(w)
+    L = []
+    cpt = 0
+    for i in range(0,borne):
+        if(i%2 == 0):
+            topology = "circle"
+        else:
+            topology = "line"
+        setTopology(topology,nb_bot_max)
+        simulatePerceptron(topology,nb_bot_max)
+        y_p = getPredictions()
+        Z = -np.ones(y_p.shape[0]).astype(int)
+        if(i%2 == 1):
+            Z = np.ones(y_p.shape[0]).astype(int)
+        P = np.max(y_p,axis=1)
+        P[np.where(np.argmax(y_p,axis=1) == 0)] = -P[np.where(np.argmax(y_p,axis=1) == 0)]
+        c=hinge_loss(P,Z)
+        cpt = cpt + c
+        print('Sanction pour une topologie : ',c)
+    print("Pénalité totale : ",cpt)
+    return cpt
+
 
 
 def validation(nb_bot_max):
@@ -167,7 +214,7 @@ def validation(nb_bot_max):
         setTopology(topology,nb_bot_max)
         simulatePerceptron(topology,nb_bot_max)
         y_p = getPredictions()
-        Y = [1 if y_p[j] > 0.5 else 0 for j in range(y_p.shape[0])]
+        Y = np.argmax(y_p)
         print("VECTEUR PREDICTIONS : ",Y)
         input("-----")
         T = [1 if Y[j] == concordance.index(topology) else 0 for j in range(len(Y))]
@@ -181,7 +228,7 @@ def validation(nb_bot_max):
 if (__name__=="__main__"):
     print("chemin courant : ",os.getcwd())
     w = readWeights()
-    res = cma.CMAEvolutionStrategy(w, 5).optimize(testAccuracyFixed, maxfun=20).result
+    res = cma.CMAEvolutionStrategy(w, 10).optimize(testHingeLoss, maxfun=20).result
     best_w = res[0]
     validation(20)
     #print("Historique de prédictions ",historique_y)
