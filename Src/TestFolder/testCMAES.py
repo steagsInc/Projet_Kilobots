2d5f77528b1b3c7617ca0724d43f1b43dee70f9c
Ouaguenouni Mohamed <ouaguenouni.hachemi@gmail.com>
2020-03-15 21:09:37 +0100

    Config premiers tests

--- Src/TestFolder/testCMAES.py
+++ Src/TestFolder/testCMAES.py
@@ -2,27 +2,53 @@ import os
 
 import cma
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 from Src.simulationController.predictionAccuracyMeasurement import simulator
 
 print("Début du test de l'extracteur des propriétés de l'essaim sur le chemin : ", os.getcwd())
 os.chdir("../..")
-S = simulator(nb=10)
+S = simulator(nb=30)
+
+S.Swarm.setTime(100)
+
 meilleur_precision = 0
+meilleur_fitness = 1000
+nb_exec = 0
+historique_precisions = []
+x_precisions = []
+historique_fitness = []
 
 def fitness(w):
     global meilleur_precision
+    global meilleur_fitness
+    global historique_precisions
+    global historique_fitness
+    global nb_exec
+    global x_precisions
+    nb_exec = nb_exec + 1
+    print("Etape ", nb_exec , " : ")
+
+    x_precisions.append(nb_exec)
     S.Swarm.controller.write_Weights(w)
     S.computeSimulation()
-    print("Pénalité de ",S.getHinge())
-    if(S.getPrecision() > meilleur_precision):
-        meilleur_precision = S.getPrecision()
-        print("Précision : ", S.getPrecision())
+    P = S.getPrecision()
+    L = S.getHinge()
+    historique_precisions.append(P)
+    historique_fitness.append(L)
+    if(P > meilleur_precision):
+        meilleur_precision = P
+        print("Précision : ", P)
+    if (L < meilleur_fitness):
+        meilleur_fitness = L
+        print("Penalite : ", L)
     return S.getHinge()
 
 if(__name__=="__main__"):
     S.Swarm.controller.put_Random_Weights()
     w = S.Swarm.controller.read_Weights()
     with tf.device('/GPU:0'):
-        res = cma.CMAEvolutionStrategy(w, 0.1).optimize(fitness, maxfun=1000).result
+        res = cma.CMAEvolutionStrategy(w, 0.1).optimize(fitness, maxfun=500).result
+    plt.plot(x_precisions, historique_precisions, color='green')
+    plt.plot(x_precisions, historique_fitness, color='red')
+    plt.show()
     print("Fin de l'executin : meilleur précision : ",meilleur_precision)
--- Src/controllers/simulationController.py
+++ Src/controllers/simulationController.py
@@ -124,15 +124,12 @@ class simulationController:
     def run(self):
         if(self.path_project and self.path_project.split("/")[-1] != os.getcwd().split("/")[-1]):
             os.chdir(self.path_project)
-        print("Avant bug je suis : ",os.getcwd())
         if (self.path != os.getcwd().split("/")[-1]):
-            print("Changement de chemin in")
             os.chdir("embedded_simulateurs/" + self.path)
         json.dump(self.parametres,open("kilombo.json","w"))
         os.system("make >error.tmp 2>&1")
         os.system("./"+self.path+" >error.tmp 2>&1")
         if (self.path == os.getcwd().split("/")[-1]):
-            print("Changement de chemin out")
             os.chdir("../..")
         return self
 
